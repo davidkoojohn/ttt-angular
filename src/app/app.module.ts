11769c4992b35cee5b0e6c9ac97b8f8d61da8444
guozhao <davidkoojohn@163.com>
2021-12-25 18:19:04 +0800

    create game-* component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { GameComponent } from './game/game.component';
+import { GameBoardComponent } from './game/game-board/game-board.component';
+import { GameSquareComponent } from './game/game-square/game-square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    GameBoardComponent,
+    GameSquareComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game/game-square/game-square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-square',
+  templateUrl: './game-square.component.html',
+  styleUrls: ['./game-square.component.less']
+})
+export class GameSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
