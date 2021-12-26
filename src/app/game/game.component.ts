0f6fd38aca91e7054e2832e7c3670205ae2aa703
guozhao <davidkoojohn@163.com>
2021-12-26 18:16:44 +0800

    calc winner

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type TWinner = {
+  val: string
+  pos: number[]
+}
 
 @Component({
   selector: 'app-game',
@@ -10,19 +15,48 @@ export class GameComponent implements OnInit {
   constructor() {}
 
   private xIsNextPlayer: boolean = true
+  private scores: number[] = [8, 1, 6, 3, 5, 7, 4, 9, 2]
   squares: string[] = new Array(9).fill(null)
-  title: string = "Next player is: X"
+  title: string = ""
 
   ngOnInit(): void {
   }
 
+  ngDoCheck() {
+    const winner = this.calculateWinner()
+    if(!winner) {
+      this.title = `Next player is: ${ this.xIsNextPlayer ? "X" : "O"}`
+    } else {
+      this.title = `Winner is: ${ winner.val }`
+    }
+  }
+
   handleClick(index: number) {
+    if (this.squares[index] || this.calculateWinner()) {
+      return
+    }
     this.squares[index] = this.xIsNextPlayer ? "X" : "O"
     this.xIsNextPlayer = !this.xIsNextPlayer
-    if (this.xIsNextPlayer) {
-      this.title = "Next player is: X"
-    } else {
-      this.title = "Next player is: O"
+  }
+
+  calculateWinner(): TWinner {
+    let result!: TWinner
+    for (let i = 0; i < this.squares.length; i++) {
+      for (let j = 0; j < this.squares.length; j++) {
+        for (let k = 0; k < this.squares.length; k++) {
+          if (
+            this.squares[i] && this.squares[j] && this.squares[k] &&
+            (i > j &&  j > k) &&
+            (+this.scores[i] + +this.scores[j] + +this.scores[k] === 15)) {
+            result = {
+              val: this.squares[i],
+              pos: [i, j, k]
+            }
+            break
+          }
+        }
+      }
     }
+    return result
   }
 }
